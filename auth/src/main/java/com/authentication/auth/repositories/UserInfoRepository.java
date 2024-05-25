fb402d38ad36ad06f5a12bff066448589c26ead0
uditGit2002 <bahukhandiu@gmail.com>
2024-05-25 12:24:07 +0530

    authentication Spring Boot 3.2

---
+++ auth/src/main/java/com/authentication/auth/repositories/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.authentication.auth.repositories;
+
+import com.authentication.auth.model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+        Optional<UserInfo>findByName(String username);
+        }
